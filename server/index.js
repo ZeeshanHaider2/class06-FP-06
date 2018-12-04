679e4e02bf3d049425dd116abf56a504007b9f11
marco_dca <marcodecara@gmail.com>
2018-12-04 23:29:02 +0100

    test

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.get('/api', Routes );
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'/../client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
