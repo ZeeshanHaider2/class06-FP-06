af87f43e0426aa2106ab07bde47f3ef78f12bfe6
marco_dca <marcodecara@gmail.com>
2018-12-05 20:34:55 +0100

    simple set-up

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      mentors: []
+    }
+  }
+
+   getAllMentors= async () => {
+
+    const mentors = await fetch(`${window.location.origin}/mentors`);
+
+    this.setState(prevState => ({
+      ...prevState, mentors: mentors
+    }), console.log('Mentors fetched and added to state')
+    );
+  }
+
   render() {
     return (
       <div className="App">
-        <p>Testing!</p>
+        <button onClick={this.getAllMentors}>Get ALL the mentors!</button>
+        {this.state.mentors.map(mentor => (
+            <div key={mentor.id}>
+              <p><b>Name:</b>{mentor.first_name}</p>
+              <p><b>Last name:</b>{mentor.last_name}</p>
+              <p><b>Github:</b><a href={mentor.slack_nickname}>{mentor.slack_nickname}</a></p>
+            </div>
+        )
+          )}
       </div>
     );
   }
